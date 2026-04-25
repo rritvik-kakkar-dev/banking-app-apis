1d5466c122029886a261ce2738ce7acece68df21
rritvik-kakkar-dev <rritvik98kakkar@gmail.com>
2026-04-25 16:06:47 +0530

    RBAC implementation

--- src/test/java/com/banking/banking_app_apis/service/impl/UserServiceImplTest.java
+++ src/test/java/com/banking/banking_app_apis/service/impl/UserServiceImplTest.java
@@ -17,7 +17,6 @@ import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
