6df3f7b395e7fecc81855d940903e62e832b7d25
rritvik-kakkar-dev <rritvik98kakkar@gmail.com>
2026-06-14 18:52:50 +0530

    fix: remove debug print statement from createAccount method

--- src/main/java/com/banking/banking_app_apis/service/impl/UserServiceImpl.java
+++ src/main/java/com/banking/banking_app_apis/service/impl/UserServiceImpl.java
@@ -54,7 +54,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public BankResponse createAccount(UserRequest userRequest) {
 
-        System.out.println(Role.ROLE_USER);
         if(userRepository.existsByEmail(userRequest.getEmail())) {
             throw new DuplicateAccountException("Account with this email already exists: "
                     + userRequest.getEmail());
